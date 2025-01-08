61a1fc25bb5493c6e57933465e907321b71d419e
CarlosAugg <138089413+CarlosAugg@users.noreply.github.com>
2025-01-08 19:14:47 -0300

    Files

---
+++ WebAtividade/FI.AtividadeEntrevista/BLL/BoCliente.cs
@@ -0,0 +1,141 @@
+﻿using FI.AtividadeEntrevista.DML;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FI.AtividadeEntrevista.BLL
+{
+    public class BoCliente
+    {
+        private static bool IsCpfValid(string cpf)
+        {
+            if (string.IsNullOrEmpty(cpf))
+                return false;
+
+            cpf = new string(cpf.Where(char.IsDigit).ToArray());
+
+            if (cpf.Length != 11)
+                return false;
+
+            if (new string(cpf[0], 11) == cpf)
+                return false;
+
+            int[] multiplicador1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplicador2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+
+            string tempCpf = cpf.Substring(0, 9);
+            int soma = 0;
+
+            for (int i = 0; i < 9; i++)
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador1[i];
+
+            int resto = soma % 11;
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+
+            tempCpf = tempCpf + resto;
+            soma = 0;
+
+            for (int i = 0; i < 10; i++)
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador2[i];
+
+            resto = soma % 11;
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+
+            return cpf.EndsWith(resto.ToString());
+        }
+        /// <summary>
+        /// Inclui um novo cliente
+        /// </summary>
+        /// <param name="cliente">Objeto de cliente</param>
+        public long Incluir(DML.Cliente cliente)
+        {
+            if (!IsCpfValid(cliente.CPF))
+            {
+                throw new Exception("O CPF informado é inválido.");
+            }
+            try
+            {
+                DAL.DaoCliente cli = new DAL.DaoCliente();
+                return cli.Incluir(cliente);
+            }
+            catch (Exception ex)
+            {
+                // Re-lança a exceção para que a camada superior possa tratá-la.
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Altera um cliente
+        /// </summary>
+        /// <param name="cliente">Objeto de cliente</param>
+        public void Alterar(DML.Cliente cliente)
+        {
+            if (!IsCpfValid(cliente.CPF))
+            {
+                throw new Exception("O CPF informado é inválido.");
+            }
+            DAL.DaoCliente cli = new DAL.DaoCliente();
+            cli.Alterar(cliente);
+        }
+
+        /// <summary>
+        /// Consulta o cliente pelo id
+        /// </summary>
+        /// <param name="id">id do cliente</param>
+        /// <returns></returns>
+        public DML.Cliente Consultar(long id)
+        {
+            DAL.DaoCliente cli = new DAL.DaoCliente();
+            return cli.Consultar(id);
+        }
+
+        /// <summary>
+        /// Excluir o cliente pelo id
+        /// </summary>
+        /// <param name="id">id do cliente</param>
+        /// <returns></returns>
+        public void Excluir(long id)
+        {
+            DAL.DaoCliente cli = new DAL.DaoCliente();
+            cli.Excluir(id);
+        }
+
+        /// <summary>
+        /// Lista os clientes
+        /// </summary>
+        public List<DML.Cliente> Listar()
+        {
+            DAL.DaoCliente cli = new DAL.DaoCliente();
+            return cli.Listar();
+        }
+
+        /// <summary>
+        /// Lista os clientes
+        /// </summary>
+        public List<DML.Cliente> Pesquisa(int iniciarEm, int quantidade, string campoOrdenacao, bool crescente, out int qtd)
+        {
+            DAL.DaoCliente cli = new DAL.DaoCliente();
+            return cli.Pesquisa(iniciarEm, quantidade, campoOrdenacao, crescente, out qtd);
+        }
+
+        /// <summary>
+        /// VerificaExistencia
+        /// </summary>
+        /// <param name="CPF"></param>
+        /// <returns></returns>
+        public bool VerificarExistencia(string CPF)
+        {
+            DAL.DaoCliente cli = new DAL.DaoCliente();
+            return cli.VerificarExistencia(CPF);
+        }
+    }
+}
---
+++ WebAtividade/FI.AtividadeEntrevista/DAL/Padrao/FI.AcessoDados.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace FI.AtividadeEntrevista.DAL
+{
+    internal class AcessoDados
+    {
+        private string stringDeConexao
+        {
+            get
+            {
+                ConnectionStringSettings conn = ConfigurationManager.ConnectionStrings["BancoDeDados"];
+                if (conn != null)
+                    return conn.ConnectionString;
+                else
+                    return string.Empty;
+            }
+        }
+
+        internal bool VerificarCpfExistente(string cpf)
+        {
+            string NomeProcedure = "VerificarCpfExistente";
+            List<SqlParameter> parametros = new List<SqlParameter>()
+            {
+             new SqlParameter("@CPF", cpf)
+            };
+
+            DataSet ds = Consultar(NomeProcedure, parametros);
+
+            if (ds != null && ds.Tables.Count > 0 && ds.Tables[0].Rows.Count > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        internal void Executar(string NomeProcedure, List<SqlParameter> parametros, string cpf = null)
+        {
+            if (NomeProcedure.ToLower() == "FI_SP_IncClienteV2".ToLower() && !string.IsNullOrEmpty(cpf))
+            {
+                if (VerificarCpfExistente(cpf))
+                {
+                    throw new Exception($"CPF {cpf} já existe!");
+                }
+            }
+
+            SqlCommand comando = new SqlCommand();
+            SqlConnection conexao = new SqlConnection(stringDeConexao);
+            comando.Connection = conexao;
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.CommandText = NomeProcedure;
+            foreach (var item in parametros)
+                comando.Parameters.Add(item);
+
+            conexao.Open();
+            try
+            {
+                comando.ExecuteNonQuery();
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+
+        internal DataSet Consultar(string NomeProcedure, List<SqlParameter> parametros)
+        {
+            SqlCommand comando = new SqlCommand();
+            SqlConnection conexao = new SqlConnection(stringDeConexao);
+
+            comando.Connection = conexao;
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.CommandText = NomeProcedure;
+            foreach (var item in parametros)
+                comando.Parameters.Add(item);
+
+            SqlDataAdapter adapter = new SqlDataAdapter(comando);
+            DataSet ds = new DataSet();
+            conexao.Open();
+
+            try
+            {
+                adapter.Fill(ds);
+            }
+            catch (SqlException ex) when (ex.Number == 2627)  // Error number for unique constraint violation
+            {
+                // Rethrow a new exception with a more specific message.
+                if (conexao != null && conexao.State == ConnectionState.Open)
+                    conexao.Close();
+                throw new Exception("Erro: Já existe um cliente cadastrado com esse CPF", ex);
+            }
+            finally
+            {
+                if (conexao != null && conexao.State == ConnectionState.Open)
+                    conexao.Close();
+            }
+
+            return ds;
+        }
+    }
+}
